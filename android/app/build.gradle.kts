ae8a9826104a37a14045ad6f4aa68bffbbadbe7b
Manoranjan-Baral <manoranjan.baral@testlify.com>
2026-06-30 16:11:17 +0530

    Update Android namespace and applicationId to match original package com.example.myapplication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.rickshaw_flutter"
+    namespace = "com.example.myapplication"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.rickshaw_flutter"
+        applicationId = "com.example.myapplication"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
